b4fb77b5466bb91b894f2ff7460a03f6869635ae
anantiikenna <ananti.ikenna@gmail.com>
2024-07-06 01:19:21 +0100

    Added `px-10` class to `section` element in AuthLayout component.

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -10,7 +10,7 @@ const AuthLayout = () => {
           <Navigate to='/' />
         ): (
           <>
-            <section className='flex flex-1 justify-center items-center flex-col py-10 ' >
+            <section className='flex flex-1 justify-center items-center flex-col py-10 px-10 ' >
               <Outlet />
             </section>
 
